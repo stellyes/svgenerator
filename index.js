61b8d93c340e993f931eaee18effa816b2688f25
Ryan England <me@rengland.org>
2023-10-21 21:26:38 -0700

    Simplified colors.json and added questions for user prompt

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-import color_module from "./utils/colors.json" assert { type: "json" };
 const inquirer = require("inquirer");
-const colors = color_module.data; // Get web-safe colors as array
+const fs = require("fs");
+
+// Get HTML colors from colors.json file
+const colors = JSON.parse(fs.readFileSync("./utils/colors.json"));
 
 const questions = [
   {
-    message: "Please input your project title: ",
-    name: "title",
+    message: "Please enter logo text (up to 3 characters): ",
+    name: "text",
     validate: function (input) {
-      return input.length >= 3;
+      return input.length <= 3 && input.length > 0;
+    },
+  },
+  {
+    message:
+      "Please choose a color for your logo text (HTML5 color keyword or hexadecimal value): ",
+    name: "color-text",
+    validate: function (input) {
+      // Checks if input is HTML Color, 3 digit hex value, or 6 digit hex value
+      return isColor(input) || input.length === 3 || input.length === 6;
+    },
+  },
+  {
+    type: "list",
+    message: "Please choose a shape for your logo: ",
+    name: "shape",
+    choices: ["Square", "Circle", "Triangle"],
+  },
+  {
+    message:
+      "Please choose a color for your shape (HTML5 color keyword or hexadecimal value): ",
+    name: "color-shape",
+    validate: function (input) {
+      // Checks if input is HTML Color, 3 digit hex value, or 6 digit hex value
+      return isColor(input) || input.length === 3 || input.length === 6;
     },
   },
 ];
 
-// Checks if input color name is in web-safe color array
+// Checks if input color name is in HTML color array
 function isColor(name) {
   for (const color of colors) {
-    if (color.name.toLowerCase() === name.toLowerCase()) {
+    if (colortoLowerCase() === name.toLowerCase()) {
       return true;
     }
   }
@@ -23,7 +49,9 @@ function isColor(name) {
 }
 
 function init() {
-  return;
+  inquirer.prompt(questions).then(function (answers) {
+    console.log(answers);
+  });
 }
 
 init();
