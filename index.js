cedd6d5711b7d0747644ea235c141aab569f4e5a
Ryan England <me@rengland.org>
2023-10-21 19:56:49 -0700

    Added color keyword check

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+import color_module from "./utils/colors.json" assert { type: "json" };
 const inquirer = require("inquirer");
+const colors = color_module.data; // Get web-safe colors as array
 
-const questions = [{}];
+const questions = [
+  {
+    message: "Please input your project title: ",
+    name: "title",
+    validate: function (input) {
+      return input.length >= 3;
+    },
+  },
+];
+
+// Checks if input color name is in web-safe color array
+function isColor(name) {
+  for (const color of colors) {
+    if (color.name.toLowerCase() === name.toLowerCase()) {
+      return true;
+    }
+  }
+  return false;
+}
 
 function init() {
   return;
